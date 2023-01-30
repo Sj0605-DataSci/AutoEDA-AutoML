e0ea32133bb443d4b06a3087853f4afce77a7dce
Sanyam Jain <64300568+Sj0605-DataSci@users.noreply.github.com>
2023-01-30 14:17:13 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,123 @@
+import streamlit as st
+import pandas as pd
+from lazypredict.Supervised import LazyClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_breast_cancer
+import matplotlib.pyplot as plt
+import seaborn as sns
+import base64
+import io
+
+#---------------------------------#
+# Page layout
+## Page expands to full width
+st.set_page_config(page_title='The Machine Learning Algorithm Comparison App',
+    layout='wide')
+#---------------------------------#
+
+# Model building
+def build_model(df):
+    df = df.loc[:100] # FOR TESTING PURPOSE, COMMENT THIS OUT FOR PRODUCTION
+    X = df.iloc[:,:-1] # Using all column except for the last column as X
+    Y = df.iloc[:,-1] # Selecting the last column as Y
+
+    st.markdown('**1.2. Dataset dimension**')
+    st.write('X')
+    st.info(X.shape)
+    st.write('Y')
+    st.info(Y.shape)
+
+    st.markdown('**1.3. Variable details**:')
+    st.write('X variable (first 20 are shown)')
+    st.info(list(X.columns[:20]))
+    st.write('Y variable')
+    st.info(Y.name)
+
+    # Build lazy model
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y,test_size = split_size,random_state = seed_number)
+    clf = LazyClassifier(verbose=0, ignore_warnings=False, custom_metric=None)
+    models_train, predictions_train = clf.fit(X_train, X_train, Y_train, Y_train)
+    models_test, predictions_test = clf.fit(X_train, X_test, Y_train, Y_test)
+
+    st.subheader('2. Table of Model Performance')
+
+    st.write('Training set')
+    st.write(predictions_train)
+    st.markdown(filedownload(predictions_train,'training.csv'), unsafe_allow_html=True)
+
+    st.write('Test set')
+    st.write(predictions_test)
+    st.markdown(filedownload(predictions_test,'test.csv'), unsafe_allow_html=True)
+
+    st.subheader('3. Plot of Model Performance (Test set)')
+
+    with st.markdown('**Accuracy Score**'):
+        # Tall
+        plt.figure(figsize=(3, 9))
+        sns.set_theme(style="whitegrid")
+        ax1 = sns.barplot(x=predictions_test.index, y="Accuracy", data=predictions_test)
+        ax1.set(xlim=(0, 1))
+    st.markdown(imagedownload(plt,'plot-acc-tall.pdf'), unsafe_allow_html=True)
+    plt.figure(figsize=(9, 12))
+    sns.set_theme(style="whitegrid")
+    ax2 = sns.barplot(y=predictions_test.index, x="Accuracy", data=predictions_test)
+    st.pyplot(plt)
+    st.markdown(imagedownload(plt,'plot-acc-wide.pdf'), unsafe_allow_html=True)
+
+def filedownload(df, filename):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}.csv">Download {filename}.csv</a>'
+    return href
+
+def imagedownload(plt, filename):
+    s = io.BytesIO()
+    plt.savefig(s, format='pdf', bbox_inches='tight')
+    plt.close()
+    b64 = base64.b64encode(s.getvalue()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:image/png;base64,{b64}" download={filename}>Download {filename} File</a>'
+    return href
+
+st.write("""
+# The Machine Learning Algorithm Comparison App
+In this implementation, the **lazypredict** library is used for building several machine learning classification models at once.
+""")
+
+#---------------------------------#
+# Sidebar - Collects user input features into dataframe
+with st.sidebar.header('1. Upload your CSV data'):
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+    st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
+""")
+
+# Sidebar - Specify parameter settings
+with st.sidebar.header('2. Set Parameters'):
+    split_size = st.sidebar.slider('Data split ratio (% for Training Set)', 10, 90, 80, 5)
+    seed_number = st.sidebar.slider('Set the random seed number', 1, 100, 42, 1)
+
+
+#---------------------------------#
+# Main panel
+
+# Displays the dataset
+st.subheader('1. Dataset')
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.markdown('**1.1. Glimpse of dataset**')
+    st.write(df)
+    build_model(df)
+else:
+    st.info('Awaiting for CSV file to be uploaded.')
+    if st.button('Press to use Example Dataset'):
+        # Breast cancer dataset
+        breast = load_breast_cancer()
+        X = pd.DataFrame(breast.data, columns=breast.feature_names).loc[:100] # FOR TESTING PURPOSE, COMMENT THIS OUT FOR PRODUCTION
+        Y = pd.Series(breast.target, name='target').loc[:100] # FOR TESTING PURPOSE, COMMENT THIS OUT FOR PRODUCTION
+        df = pd.concat( [X,Y], axis=1 )
+
+        st.markdown('The Breast cancer dataset is used as the example.')
+        st.write(df.head(5))
+
+        build_model(df)
